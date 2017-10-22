4bb1264e702a34f500292d6fea96965a378b678c
Ulric Wilfred <shaman.sir@gmail.com>
2017-10-22 19:34:59 +0200

    try adding CodeMirror instances

--- index.js
+++ index.js
@@ -2,18 +2,37 @@
 
 require('codemirror/lib/codemirror.css');
 require('codemirror/theme/monokai.css');
+require('./index.css');
 
 require('whatwg-fetch');
 
 var Counter = require('./Counter.elm');
 var mountNode = document.getElementById('elm-app');
-var Codemirror = require('codemirror');
+var CodeMirror = require('codemirror');
 require('codemirror/mode/elm/elm');
 
-console.log(Codemirror);
+var instanceCount = 0;
+
+function addInstance(target) {
+    console.log('adding');
+    var codemirrorWrapper = document.createElement('div');
+    var previewInstance = document.createElement('div');
+    target.appendChild(codemirrorWrapper);
+    target.appendChild(previewInstance);
+    var codemirrorInstance = CodeMirror(codemirrorWrapper, {
+        value: "foo = \"bar\"",
+        mode:  "elm"
+    });
+    codemirrorWrapper.focus();
+    instanceCount++;
+}
+
+//document.addEventListener('DOMContentLoaded', function() {
+    addInstance(document.body);
+//});
 
 // The third value on embed are the initial values for incomming ports into Elm
-var app = Counter.Main.embed(mountNode);
+//var app = Counter.Main.embed(mountNode);
 
 fetch('http://localhost:3000')
     .then(function(response) {
