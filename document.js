4ee78668b8690608907fb231c52b3375da67a49f
Ulric Wilfred <shaman.sir@gmail.com>
2017-10-29 20:26:20 +0100

    properly extract definitions + join all the definitions and imports in Prelude

--- document.js
+++ document.js
@@ -14,28 +14,42 @@ class RevlDocument {
   append(cellId, content) {
     this.definitions[cellId] = [];
     this.chunks[cellId] = [];
+
+    let inDefinition = false;
+    let currentDefinition = '';
+
     content.split('\n').forEach(line => {
       if (isImport(line)) {
-          // FIXME: only if this import is known library
-          this.imports = unique(this.imports.concat([ line.slice(8) ]));
-      } else if (line.indexOf('import ') < 0) {
-          this.chunks[cellId].push(line);
+        // FIXME: only if this import is known library
+        this.imports = unique(this.imports.concat([ line.slice(8) ]));
+      } else if (!inDefinition && (isTypeDeclaration(line) || isDefinition(line))) {
+        inDefinition = true;
+        this.definitions[cellId].push(line);
+      } else if (inDefinition) {
+        if (line.length == 0) {
+          this.definitions[cellId].push('');
+          inDefinition = false;
+        } else {
+          this.definitions[cellId].push(line);
+        }
+      } else {
+        this.chunks[cellId].push(line);
       }
     })
   }
 
   buildPreludeFor(cellId) {
     // FIXME: do not compile Prelude if definitions are empty
-    if (!this.definitions[cellId].length) return [ 'foo = "bar"' ];
-    return []
-      .concat(
-        this.imports
-            .map((importBody) => 'import ' + importBody)
+    const allDefinitions = this.definitions.map(cellDefs => cellDefs.join('\n'));
+    const hasDefinitions = allDefinitions.join('\n').length > 0;
+    return this.imports
+      .map(
+        importBody => 'import ' + importBody
+      ).concat(
+        [ '' ]
+      ).concat(
+        hasDefinitions ? allDefinitions : [ 'foo = "bar"' ]
       )
-      .concat([ '' ])
-      .concat(
-        this.definitions[cellId].map((lines) => lines.join('\n'))
-      );
   }
 
   buildViewerFor(cellId) {
@@ -61,11 +75,11 @@ function isImport(line) {
 }
 
 function isTypeDeclaration(line) {
-  return line.match(/^\w+\s*:/) == 0;
+  return line.match(/^\w+\s*:/) != null;
 }
 
 function isDefinition(line) {
-  return line.match(/^\w+\s*=/) == 0;
+  return line.match(/^\w+\s*=/) != null;
 }
 
 module.exports = RevlDocument;
