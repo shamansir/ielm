1d34a774f1c10e419deb78effec0692878eff9cb
Ulric Wilfred <shaman.sir@gmail.com>
2017-11-05 17:14:08 +0100

    add TypeType component, to render type structures

---
+++ Component/TypeType.elm
@@ -0,0 +1,36 @@
+module Component.TypeType exposing (render, TypeAtom)
+
+-- See https://github.com/shamansir/node-elm-repl/blob/master/Types.md as a reference
+
+import List
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+type TypeAtom
+    = Name String
+    | Variable String
+    | Lambda TypeAtom TypeAtom
+    | App TypeAtom (List TypeAtom)
+    | Record (List (String, TypeAtom))
+
+render : TypeAtom -> Html msg
+render atom =
+    case atom of
+        Name n -> span [ class "type_atom" ] [ text n ]
+        Variable v -> span [ class "type_var" ] [ text v ]
+        Lambda left right ->
+            div [ class "type_lambda" ]
+                [ render left, render right ]
+        App subj obj ->
+            div [ class "type_app" ]
+                ((render subj) :: (obj |> List.map render))
+        Record fields ->
+            div [ class "type_record" ]
+                (fields |> List.map renderField)
+
+renderField : (String, TypeAtom) -> Html msg
+renderField ( name, atom ) =
+    span [ class "record_field" ]
+        [ span [ class "field_name" ] [ text name ]
+        , span [ class "field_value" ] [ render atom ]
+        ]
