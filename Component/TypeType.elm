f8691651092d795ec387b37d4b02f7c7511e9e9a
Ulric Wilfred <shaman.sir@gmail.com>
2017-11-05 23:19:06 +0100

    render the actual adapted type for every cell instead of a mock

--- Component/TypeType.elm
+++ Component/TypeType.elm
@@ -10,7 +10,8 @@ type TypeAtom
     = Name String
     | Variable String
     | Lambda TypeAtom TypeAtom
-    | App TypeAtom (List TypeAtom)
+    | Application TypeAtom (List TypeAtom)
+    | Alias TypeAtom (List TypeAtom)
     | Record (List (String, TypeAtom))
 
 render : TypeAtom -> Html msg
@@ -21,9 +22,12 @@ render atom =
         Lambda left right ->
             div [ class "type_lambda" ]
                 [ render left, render right ]
-        App subj obj ->
+        Application subj obj ->
             div [ class "type_app" ]
                 ((render subj) :: (obj |> List.map render))
+        Alias def list ->
+            div [ class "type_aliased" ]
+                ((render def) :: (list |> List.map render))
         Record fields ->
             div [ class "type_record" ]
                 (fields |> List.map renderField)
--- adapt-type.js
+++ adapt-type.js
@@ -1,5 +1,16 @@
-function stringifier(type) {
-    return '(App (Lambda (Name "foo") (Variable "t")) [(Name "bar"), (Variable "d")])'
+// See https://github.com/shamansir/node-elm-repl/blob/master/Types.md as a reference
+
+function stringify(t) {
+    if (t.type == 'type') return `(Name "${t.def.name}")`;
+    if (t.type == 'var') return `(Variable "${t.def.name}")`;
+    if (t.type == 'lambda') return `(Lambda ${stringify(t.left)} ${stringify(t.right)})`;
+    if (t.type == 'app') return `(Application ${stringify(t.subject)} [ ${t.object.map(stringifier).join(', ')} ])`;
+    if (t.type == 'aliased') return `(Alias ${stringify(t.subject)} [ ${t.object.map(stringifier).join(', ')} ])`;
+    if (t.type == 'record') return `(Record [ ${t.fields.map(stringifyField).join(', ')} ])`;
 }
 
-module.exports = stringifier;
+function stringifyField(f) {
+    return `( "${f.name}", ${stringify(f.node)} )`;
+}
+
+module.exports = stringify;
