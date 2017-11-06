4d21c64e9be3bf524033447a128d0068d580a1c5
Ulric Wilfred <shaman.sir@gmail.com>
2017-11-06 14:17:49 +0100

    render record types including fields and their types

--- src/server/adapt-type.js
+++ src/server/adapt-type.js
@@ -4,10 +4,10 @@ function stringify(t) {
     if (t.type == 'type') return `(Name "${t.def.name}")`;
     if (t.type == 'var') return `(Variable "${t.name}")`;
     if (t.type == 'lambda') return `(Lambda ${stringify(t.left)} ${stringify(t.right)})`;
-    if (t.type == 'app') return `(Application ${stringify(t.subject)} [ ${t.object.map(stringifier).join(', ')} ])`;
+    if (t.type == 'app') return `(Application ${stringify(t.subject)} [ ${t.object.map(stringify).join(', ')} ])`;
     if (t.type == 'aliased') {
         if (t.msgvar) return `(MessageAlias "${t.def.name}" "${t.msgvar}")`;
-        return `(Alias "${t.def.name}" [ ${t.list.map(stringifier).join(', ')} ])`;
+        return `(Alias "${t.def.name}" [ ${t.list.map(stringify).join(', ')} ])`;
     }
     if (t.type == 'record') return `(Record [ ${t.fields.map(stringifyField).join(', ')} ])`;
 }
