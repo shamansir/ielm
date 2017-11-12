ea49a523fe760c1a3991fa21a68258cd748c7b2d
Ulric Wilfred <shaman.sir@gmail.com>
2017-11-12 23:09:52 +0100

    minor style improvements for App component

--- src/server/Component/AppType.elm
+++ src/server/Component/AppType.elm
@@ -23,19 +23,30 @@ render appName objectsCount objectRenderer item =
 
 renderMaybe : (x -> Html a) -> Maybe x -> Html a
 renderMaybe renderJust maybeVal =
-    div [ class "comp_app" ]
+    div [ class "comp_app comp_app--maybe" ]
         [ div [ class "comp_app_name" ] [ text "Maybe" ]
         , case maybeVal of
-            Just v -> div [ class "comp_app_object" ] [ renderJust v ]
-            Nothing -> div [ class "comp_app_object comp_app--failure-text" ] [ text "Nothing" ]
+            Just v -> div [ class "comp_app_object" ]
+                          [ span [ class "comp_app_variant" ] [ text "Just" ]
+                          , renderJust v
+                          ]
+            Nothing -> div [ class "comp_app_object comp_app--failure-text" ]
+                           [ span [ class "comp_app_variant" ] [ text "Nothing" ]
+                           ]
         ]
 
 
 renderResult : (err -> Html a) -> (ok -> Html a) -> Result err ok -> Html a
 renderResult renderErr renderOk resultVal =
-    div [ class "comp_app" ]
+    div [ class "comp_app comp_app--result" ]
         [ div [ class "comp_app_name" ] [ text "Result" ]
         , case resultVal of
-            Ok v -> div [ class "comp_app_object" ] [ renderOk v ]
-            Err e -> div [ class "comp_app_object comp_app--failure" ] [ renderErr e ]
+            Ok v -> div [ class "comp_app_object" ]
+                        [ span [ class "comp_app_variant" ] [ text "Ok" ]
+                        , renderOk v
+                        ]
+            Err e -> div [ class "comp_app_object comp_app--failure" ]
+                         [ span [ class "comp_app_variant" ] [ text "Err" ]
+                         , renderErr e
+                         ]
         ]
